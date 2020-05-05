bfa799a6126fb1442aa6fb6aaba327b8fdb55893
Michaela DeCamara <michaela.decamara@gmail.com>
2020-05-05 04:53:43 -0400

    admin categories and products upload fail

---
+++ src/core/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "./Layout";
+
+const Home = () => (
+    <Layout title="WatchList Home" description="">
+        <img class="img-responsive" src="/images/rolex_movement.jpg" alt="Watch movement"></img>
+    </Layout>
+);
+
+export default Home;
