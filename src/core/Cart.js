9f8f9d0aa7f09d783a94956ebc510d2a765a2ada
Michaela DeCamara <michaela.decamara@gmail.com>
2020-05-09 09:59:20 -0400

    seeded database

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -5,54 +5,59 @@ import { getCart } from "./cartHelpers";
 import Card from "./Card";
 
 const Cart = () => {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        setItems(getCart());
-    }, [items]);
+  const [run, setRun] = useState(false);
 
-    const showItems = items => {
-        return (
-            <div>
-                <h2>Your cart has {`${items.length}`} items</h2>
-                <hr />
-                {items.map((product, i) => (
-                    <Card
-                        key={i}
-                        product={product}
-                        showAddToCartButton={false}
-                        cartUpdate={true}
-                        showRemoveProductButton={true}
-                    />
-                ))}
-            </div>
-        );
-    };
+  useEffect(() => {
+    // console.log(getCart());
+    setItems(getCart());
+  }, [run]);
 
-    const noItemsMessage = () => (
-        <h2>
-            Your cart is empty. <br /> 
-            <Link to="/shop">Continue shopping</Link>
-        </h2>
-    );
-
+  const showItems = items => {
     return (
-        <Layout
-            title="Shopping Cart"
-            description="Manage your cart items. Add, remove or continue shopping."
-            className="container-fluid"
-        >
-            <div className="row">
-                <div className="col-6">
-                    {items.length > 0 ? showItems(items) : noItemsMessage()}
-                </div>
-
-                <div className="col-6">
-                    <h2 className="mb-4">Your cart summary</h2>
-                </div>
-            </div>
-        </Layout>
+      <div>
+        <h2>Your cart has {`${items.length}`} items</h2>
+        <hr />
+        {items.map((product, i) => (
+          <Card
+            key={i}
+            product={product}
+            showAddToCartButton={false}
+            cartUpdate={true}
+            showRemoveProductButton={true}
+            setRun={setRun}
+            run={run}
+          />
+        ))}
+      </div>
     );
+  };
+
+  const noItemsMessage = () => (
+    <h2>
+      Your cart is empty. <br />
+      <Link to="/shop">Continue shopping</Link>
+    </h2>
+  );
+
+  return (
+    <Layout
+      title="Shopping Cart"
+      description="Manage your cart items. Add, remove or continue shopping."
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className="col-6">
+          {items.length > 0 ? showItems(items) : noItemsMessage()}
+        </div>
+
+        <div className="col-6">
+          <h2 className="mb-4">Your cart summary</h2>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Cart;
